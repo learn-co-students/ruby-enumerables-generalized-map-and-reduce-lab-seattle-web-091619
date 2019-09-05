76fc6cdca421a0c7313906e19bac1ad9cbfb6562
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2019-09-05 21:45:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-def my_own_map([1, 2, 3, -9]) {|n| n * -n}
+def map(arr)
+  new = []
+  i = 0
+  while i < arr.length do
+    new.push(yield(arr[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(arr, sp=nil)
+  if sp
+    sum = sp
+    i = 0
+  else
+    sum = arr[0]
+    i = 1
+  end
+
+  while i < arr.length
+    sum = yield(sum, arr[i])
+    1 += 1
+  end
+  sum
+end
