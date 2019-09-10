16decc42674045858ba807295aeb85b6fc958874
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-10 21:55:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each do |i|
+    new_array.push(yield(i))
+  end
+  new_array
+end
+
+def reduce(source_array, starting_value = 0)
+  source_array.each do |i|
+    starting_value = yield(i,starting_value)
+  end
+  if !starting_value
+    return false
+  end
+  starting_value
+end
